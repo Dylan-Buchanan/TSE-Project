ecb00f1d24b6082b8901e717140b70e6879ac11f
Dylan Buchanan <dylanbuchanan@comcast.net>
2023-02-03 15:53:49 -0500

    Added comments to pagedir.c

--- common/pagedir.c
+++ common/pagedir.c
@@ -1,6 +1,12 @@
 /*
 *
-* pagedir.c - 
+* pagedir.c - Adds 2 functions:
+* 
+* First function creates a .crawler file in the directory given
+* and returns true or false depending if it could access the directory
+*
+* Second function creates a file and writes the information of the webpage
+* passed into the function into that file
 *
 * Dylan Buchanan, Winter 2023
 */
@@ -20,14 +26,18 @@ void pagedir_save(const webpage_t* page, const char* pageDirectory, const int do
 *  Closes the file and return true.
 */
 bool pagedir_init(const char* pageDirectory) {
+    // allocate memory for the full file pathname and build the file pathname
     char* pathname = "/.crawler";
     char* fileMem = mem_malloc_assert((strlen(pageDirectory) + strlen(pathname)) * sizeof(char) + 1, "ERROR: failed allocating memory for .crawler file\n");
     sprintf(fileMem, "%s%s", pageDirectory, pathname);
+    // open the file to write (if possible)
     FILE* file = fopen(fileMem, "w");
+    // otherwise directory does not exist and return false
     if (file == NULL) {
         fprintf(stderr, "ERROR: .crawler unable to open\n");
         return false;
     }
+    // close the file and free the pathname
     fclose(file);
     mem_free(fileMem);
     return true;
@@ -42,13 +52,18 @@ bool pagedir_init(const char* pageDirectory) {
 *  Closes the file
 */
 void pagedir_save(const webpage_t* page, const char* pageDirectory, const int docID) {
+    // find the size of the docID
     int idSize = snprintf(NULL, 0, "%d", docID) + 1;
+    // allocate memory for the pathname and build the pathname
     char* fileMem = mem_malloc_assert((strlen(pageDirectory) + idSize) * sizeof(char) + 1, "ERROR: failed allocating memory for saving webpage\n");
     sprintf(fileMem, "%s/%d", pageDirectory, docID);
+    // because the page directory was checked before we know the directory is valid so file will open
     FILE* file = fopen(fileMem, "w");
+    // print page information to file
     fprintf(file, "%s\n", webpage_getURL(page));
     fprintf(file, "%d\n", webpage_getDepth(page));
-    fprintf(file, "%s\n", webpage_getHTML(page));   
+    fprintf(file, "%s\n", webpage_getHTML(page));  
+    // close the file and free the pathname 
     fclose(file);
     mem_free(fileMem);
 }
